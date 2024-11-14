782436de02af218bbf98f5221d56d8cc0297ccd3
Janko.Norbert <Janko.Norbert@C19-03>
2024-11-14 20:34:33 +0100

    1.0 version

--- 2024.11.14. - Dolgozat/Form1.Designer.cs
+++ 2024.11.14. - Dolgozat/Form1.Designer.cs
@@ -29,13 +29,185 @@ namespace _2024._11._14.__Dolgozat
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tree2 = new System.Windows.Forms.PictureBox();
+            this.tree1 = new System.Windows.Forms.PictureBox();
+            this.manBody = new System.Windows.Forms.PictureBox();
+            this.manHand = new System.Windows.Forms.PictureBox();
+            this.basket1 = new System.Windows.Forms.PictureBox();
+            this.basket2 = new System.Windows.Forms.PictureBox();
+            this.Apple = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.tree2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tree1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.manBody)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.manHand)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.basket1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.basket2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Apple)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(482, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(482, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(482, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(482, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(648, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(648, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // tree2
+            // 
+            this.tree2.Location = new System.Drawing.Point(118, 126);
+            this.tree2.Name = "tree2";
+            this.tree2.Size = new System.Drawing.Size(116, 112);
+            this.tree2.TabIndex = 6;
+            this.tree2.TabStop = false;
+            // 
+            // tree1
+            // 
+            this.tree1.Location = new System.Drawing.Point(167, 234);
+            this.tree1.Name = "tree1";
+            this.tree1.Size = new System.Drawing.Size(22, 114);
+            this.tree1.TabIndex = 7;
+            this.tree1.TabStop = false;
+            // 
+            // manBody
+            // 
+            this.manBody.Location = new System.Drawing.Point(308, 254);
+            this.manBody.Name = "manBody";
+            this.manBody.Size = new System.Drawing.Size(20, 94);
+            this.manBody.TabIndex = 8;
+            this.manBody.TabStop = false;
+            // 
+            // manHand
+            // 
+            this.manHand.Location = new System.Drawing.Point(278, 287);
+            this.manHand.Name = "manHand";
+            this.manHand.Size = new System.Drawing.Size(31, 19);
+            this.manHand.TabIndex = 9;
+            this.manHand.TabStop = false;
+            // 
+            // basket1
+            // 
+            this.basket1.Location = new System.Drawing.Point(347, 321);
+            this.basket1.Name = "basket1";
+            this.basket1.Size = new System.Drawing.Size(64, 27);
+            this.basket1.TabIndex = 10;
+            this.basket1.TabStop = false;
+            // 
+            // basket2
+            // 
+            this.basket2.Location = new System.Drawing.Point(369, 301);
+            this.basket2.Name = "basket2";
+            this.basket2.Size = new System.Drawing.Size(19, 32);
+            this.basket2.TabIndex = 11;
+            this.basket2.TabStop = false;
+            // 
+            // Apple
+            // 
+            this.Apple.Location = new System.Drawing.Point(282, 272);
+            this.Apple.Name = "Apple";
+            this.Apple.Size = new System.Drawing.Size(17, 17);
+            this.Apple.TabIndex = 12;
+            this.Apple.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Apple);
+            this.Controls.Add(this.basket2);
+            this.Controls.Add(this.basket1);
+            this.Controls.Add(this.manHand);
+            this.Controls.Add(this.manBody);
+            this.Controls.Add(this.tree2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tree1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tree2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tree1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.manBody)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.manHand)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.basket1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.basket2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Apple)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox tree2;
+        private System.Windows.Forms.PictureBox tree1;
+        private System.Windows.Forms.PictureBox manBody;
+        private System.Windows.Forms.PictureBox manHand;
+        private System.Windows.Forms.PictureBox basket1;
+        private System.Windows.Forms.PictureBox basket2;
+        private System.Windows.Forms.PictureBox Apple;
     }
 }
 
--- 2024.11.14. - Dolgozat/Form1.cs
+++ 2024.11.14. - Dolgozat/Form1.cs
@@ -1,20 +1,203 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace _2024._11._14.__Dolgozat
 {
     public partial class Form1 : Form
     {
+        Timer manMoveTimer = new Timer();
+        Timer AppleDownTimer = new Timer();
+        Timer KickTimer = new Timer();
+        int ApplePCS = 0;
+        int ManMove = 0;
+        int BasketSize = 20;
+        int kicks = 10;              //  hányszor üti meg a fát
+        bool left = true;
+        bool kopp = false;
+        int utes = 0;
+        bool AppleInTheHand = true;
+        int biggerBasket = 10;
+        int fasterKick = 30;
         public Form1()
         {
             InitializeComponent();
+            tree1.BackColor = Color.Brown;
+            tree2.BackColor = Color.Green;
+            basket1.BackColor = Color.Brown;
+            basket2.BackColor = Color.Brown;
+            Apple.BackColor = Color.Red;
+            manBody.BackColor = Color.Salmon;
+            manHand.BackColor = Color.Salmon;
+            label1.Text = $"Gyűjtött almák száma: {ApplePCS}";
+            label2.Text = $"Kosár kapacitása: {BasketSize}";
+            label3.Text = "Nagyobb kosár: +5 alma";
+            label4.Text = "Gyorsabb szedés: -1 ütés";
+            button1.Text = $"{biggerBasket} alma";
+            button2.Text = $"{fasterKick} alma";
+            Apple.Visible = false;
+            this.Text = "Alma szedése";
+            Start();
+        }
+        void Start()
+        {
+            StartTimer();
+            ButtonEvents();
+        }
+        void StartTimer()
+        {
+            manMoveTimer.Interval = 30;
+            AppleDownTimer.Interval = 15;
+            KickTimer.Interval = 300;
+
+            manMoveTimer.Start();
+
+            manMoveTimer.Tick += ManMoveEvent;
+            KickTimer.Tick += KickEvent;
+        }
+        void ButtonEvents()
+        {
+            button1.Click += (s, e) =>
+            {
+                if (ApplePCS >= biggerBasket)
+                {
+                    ApplePCS -= biggerBasket;
+                    BasketSize += 5;
+                    biggerBasket += 2;
+                    PointDraw();
+                }
+            };
+            button2.Click += (s, e) =>
+            {
+                if (ApplePCS >= fasterKick && kicks > 3)
+                {
+                    ApplePCS -= fasterKick;
+                    kicks -= 1;
+                    fasterKick += 30;
+                    PointDraw();
+                }
+            };
+        }
+        void ManMoveEvent(object s, EventArgs e)
+        {
+            if (left)
+            {
+                ManMove = -3;
+                AppleInTheHand = true;
+                manHand.Left = manBody.Right - manBody.Width - manHand.Width;
+                if (manBody.Left > tree1.Right + manHand.Width)
+                {
+                    manBody.Left += ManMove;
+                    manHand.Left = manHand.Left + ManMove;
+                    left = true;
+                }
+                else
+                {
+                    manMoveTimer.Stop();
+                    KickTimer.Start();
+                    left = false;
+                }
+            }
+            else
+            {
+                ManMove = 3;
+                manHand.Left = manBody.Right;
+                Apple.Left = manBody.Right + manHand.Width / 2;
+                Apple.Visible = true;
+                if (manBody.Right < basket1.Left - manHand.Width / 2)
+                {
+                    manBody.Left = manBody.Left + ManMove;
+                    manHand.Left = manHand.Left + ManMove;
+                    Apple.Left = Apple.Left + ManMove;
+                    left = false;
+                }
+                else
+                {
+                    manMoveTimer.Stop();
+                    AppleInTheHand = false;
+                    Apple.Visible = false;
+                    AppleDownTimer.Start();
+                    left = true;
+                    if (BasketSize > ApplePCS)
+                    {
+                        ApplePCS += 1;          // ennyivel nő az alma a kosárban
+                        PointDraw();
+                    }
+                }
+            }
+        }
+        void KickEvent(object s, EventArgs e)
+        {
+            if (kicks == utes)
+            {
+                KickTimer.Stop();
+                utes = 0;
+                AppleEvent();
+            }
+            else
+            {
+                if (kopp)
+                {
+                    manHand.Left = manHand.Left - manHand.Width;
+                    kopp = false;
+                    utes += 1;
+                }
+                else
+                {
+                    manHand.Left = manHand.Left + manHand.Width;
+                    kopp = true;
+                }
+            }
+        }
+        void AppleEvent()
+        {
+            PictureBox NewApple = new PictureBox();
+            this.Controls.Add(NewApple);
+            NewApple.Size = new Size(17, 17);
+            NewApple.Left = 196;
+            NewApple.Top = 221;
+            NewApple.BackColor = Color.Red;
+
+            AppleDownTimer.Start();
+            AppleDownTimer.Tick += (s, e) =>
+            {
+                if (AppleInTheHand)
+                {
+                    if (NewApple.Bottom >= manHand.Top)
+                    {
+                        AppleDownTimer.Stop();
+                        NewApple.Visible = false;
+                        manMoveTimer.Start();
+                    }
+                    else
+                    {
+                        NewApple.Top += 3;
+                    }
+                }
+                else
+                {
+                    NewApple.Left = Apple.Left;
+                    NewApple.Visible = true;
+                    if (NewApple.Bottom >= basket1.Top)
+                    {
+                        AppleDownTimer.Stop();
+                        NewApple.Top = 221;
+                        this.Controls.Remove(NewApple);
+                        manMoveTimer.Start();
+                    }
+                    else
+                    {
+                        NewApple.Top += 3;
+                    }
+                }
+            };
+        }
+        void PointDraw()
+        {
+            label1.Text = $"Gyűjtött almák száma: {ApplePCS}";
+            label2.Text = $"Kosár kapacitása: {BasketSize}";
+            button1.Text = $"{biggerBasket} alma";
+            button2.Text = $"{fasterKick} alma";
         }
     }
 }
